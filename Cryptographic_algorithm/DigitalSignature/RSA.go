8d896391c4ca76b88f98059a1bb2ffa4edb81aa8
Amina Yambulatova <yambulatova.amina@gmail.com>
2022-06-13 17:09:46 +0300

    add encr/decr to RSA

--- Cryptographic_algorithm/DigitalSignature/RSA.go
+++ Cryptographic_algorithm/DigitalSignature/RSA.go
@@ -5,28 +5,25 @@ import (
 	"math/big"
 )
 
-func KeyGen() { //P, Q string
-	var p, q, n, phi *big.Int //p,q - primary
-	p = big.NewInt(10001)
-	q = big.NewInt(253)
-	fmt.Println(p, q)
+func KeyGen() (*big.Int, *big.Int, *big.Int) {
+	var p, q *big.Int //p,q - prime
+	p, _ = big.NewInt(0).SetString("3", 10)
+	q, _ = big.NewInt(0).SetString("13", 10)
 
-	//A prime number is a natural number greater than 1 that has no positive divisors other than 1 and itself
-	//n.Mul(big.NewInt(-1), big.NewInt(0))
-	p.Add(p, big.NewInt(-10))
+	n := big.NewInt(0).Mul(p, q)
+	p.Add(p, big.NewInt(-1))
 	q.Add(q, big.NewInt(-1))
-	fmt.Println(p, q, n)
-	phi = phi.Mul(p, q) //Euler totient function
+
+	phi := big.NewInt(0).Mul(p, q) //Euler function
+
 	e := getE(phi)
 	d := getD(e, phi)
-	fmt.Println(d, n, e, n)
-	//PrivateKey(d, n)
-	//PublicKey(e, n)
+
+	fmt.Println("PrivateKey", d, n, "PublicKey", e, n, phi)
+	return d, e, n
 }
 
 func getE(phi *big.Int) *big.Int {
-	// Выбирается целое число e ( 1 < e < t ) // взаимно простое со значением функции Эйлера (t)
-
 	e := big.NewInt(2)
 	for {
 		if Coprime(e, phi) { //proverka na coprime
@@ -40,51 +37,43 @@ func getE(phi *big.Int) *big.Int {
 
 	return big.NewInt(-1)
 }
-func Coprime(a, b *big.Int) bool {
-	// Set a to gcd(a,b)
-	var t, x, m *big.Int
+
+func Coprime(a, b *big.Int) bool { //finds coprime number
+	var t *big.Int
 	for {
 		t = b
-		x.DivMod(a, b, m)
+		_, m := big.NewInt(0).DivMod(a, b, new(big.Int))
 		b = m
 		a = t
 		if b.Cmp(big.NewInt(0)) == 0 {
 			break
 		}
 	}
-	// By definition, a and b are coprime if gcd(a,b) == 1
 	return a.Cmp(big.NewInt(1)) == 0
 }
 
-func getD(e, phi *big.Int) *big.Int { //bruteforce
-	// Вычисляется число d, мультипликативно обратное к числу e по модулю φ(n), то есть число, удовлетворяющее сравнению:
-	//    d ⋅ e ≡ 1 (mod φ(n))
+func getD(e, phi *big.Int) *big.Int { //bruteforce  d ⋅ e ≡ 1 (mod φ(n))
 	x := big.NewInt(1)
-	var z, m1, m2, m *big.Int
-	for x.Cmp(phi) <= 0 {
-		//m1 - e%phi
-		//m2 - x%phi
-		//m -(m1*m2)%phi
-		z.DivMod(e, phi, m1)
-		z.DivMod(x, phi, m2)
-		z.DivMod(m1, m2, m)
+	for {
+		ans, m := big.NewInt(0).DivMod(x, e, new(big.Int))
 
-		if m.Cmp(big.NewInt(1)) == 0 {
-			return x
+		if m.Cmp(big.NewInt(0)) == 0 {
+			return ans
 		}
-		x.Add(x, big.NewInt(1))
+		x.Add(x, phi)
 	}
-	return big.NewInt(-1)
 }
 
-/*
-func Encrypt(message string, PublicKey int) {
+func Encrypt(message, e, n *big.Int) *big.Int {
 	return powmod(message, e, n)
 }
 
-func Decrypt(ciphertext string, PrivateKey int) {
+func Decrypt(ciphertext, d, n *big.Int) *big.Int {
 	return powmod(ciphertext, d, n)
 }
 
-
-*/
+func powmod(message, a, b *big.Int) *big.Int {
+	message.Exp(message, a, nil)
+	_, m := big.NewInt(0).DivMod(message, b, new(big.Int))
+	return m
+}
--- Cryptographic_algorithm/main.go
+++ Cryptographic_algorithm/main.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"Cryptographic_algorithm/DigitalSignature"
 	"Cryptographic_algorithm/VigenereCipher"
 	"fmt"
+	"math/big"
 )
 
 func main() {
+	//Vigenere
 	//please, enter your plaintext and key ONLY lowercase letters :)
+	//1st word in args is plaintext, 2nd - key
 	encodeResult := VigenereCipher.MyChipher("helloworld", "blockchain")
-	fmt.Println("Ciphertext:", encodeResult)
+	fmt.Println("Vigenere Ciphertext:", encodeResult)
+
+	//RSA
+	d, e, n := DigitalSignature.KeyGen()
+	encr := DigitalSignature.Encrypt(big.NewInt(123), e, n)
+	decr := DigitalSignature.Decrypt(big.NewInt(15), d, n)
+	fmt.Println("Cipher:", encr, "Plain:", decr)
+
 }
